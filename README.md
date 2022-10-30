a120dace9318b041627bcfce40907d5eed828841
LinkWHorter <vlad_kovakev12@ukr.net>
2022-10-30 17:30:53 +0200

    IDK-7: pop-upped things

---
+++ README.md
@@ -0,0 +1,1 @@
+# IDK-7: pop-uped..
--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
   $("#btn-up").click(function() {
-    $("#pop-cont").fadeIn(405);
+    $("#pop-cont,.mask").hide().fadeIn(405);
+  });
+
+  $("#btn-close,.mask").click(function() {
+    $("#pop-cont,.mask").fadeOut(405);
   })
 
-  $("#btn-close").click(function() {
-    $("#pop-cont").fadeOut(405);
-  })
+  //close the POPUP if the button with id="close" is clicked
+  /*$("#btn-close,.mask").on("click", function (e) {
+    e.preventDefault();
+    $("#pop-cont,.mask").fadeOut(405);
+  });*/
+  
 })
