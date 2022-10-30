9af0f9d4085f7ac2272d3919ba203648dfc26690
LinkWHorter <vlad_kovakev12@ukr.net>
2022-10-30 03:11:48 +0200

    IDK-7: That's pop-upped;

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#btn-up").click(function() {
+    $("#pop-cont").fadeIn(405);
+  })
+
+  $("#btn-close").click(function() {
+    $("#pop-cont").fadeOut(405);
+  })
+})
